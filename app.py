5447e8787f3697ffd37d39cd44775779d6db0a49
ENISSAY39 <yassinebsofien@gmail.com>
2026-07-17 14:55:00 +0200

    feat: normalize header

--- app.py
+++ app.py
@@ -455,6 +455,7 @@ def create_app():
                 "programs": programs_list,
                 "school_years": school_years,
                 "teachers_list": sorted(list(teachers)),
+                "user": user,
             },
         )
 
@@ -965,6 +966,7 @@ def create_app():
                 "module_attendance_question": module_attendance_question,
                 "survey_is_closed": survey_is_closed,
                 "user_has_answered": user_has_answered,
+                "user": user,
             },
         )
 
